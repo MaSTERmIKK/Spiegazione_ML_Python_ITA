1b123c4cdc51ed5e968e697d6fdb76c325d7a6c5
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2025-03-25 12:46:27 +0100

    Create train_logistic.py

---
+++ ModelliML_Singoli/train_logistic.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Carica il dataset di training dal file NPZ, addestra un modello di Logistic Regression
+e salva il modello in 'logistic_model.pkl'.
+"""
+
+import numpy as np
+import pickle
+from sklearn.linear_model import LogisticRegression
+
+def load_training_data(filename="dataset.npz"):
+    data = np.load(filename)
+    return data["X_train"], data["y_train"]
+
+def train_and_save_model(X_train, y_train, model_filename="logistic_model.pkl"):
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    with open(model_filename, "wb") as f:
+        pickle.dump(model, f)
+    print("Modello Logistic Regression addestrato e salvato in", model_filename)
+
+if __name__ == '__main__':
+    X_train, y_train = load_training_data()
+    train_and_save_model(X_train, y_train)
